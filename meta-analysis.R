24c2b88c07c3086358edd9c66ee4a59ee0344eef
Kathleen Owen <kathleenowen@MacBook-Air-479.local>
2025-03-26 20:15:46 +0000

    updating forest plot

--- meta-analysis.R
+++ meta-analysis.R
@@ -200,10 +200,14 @@ meta1
 meta2<-rma.mv(yi=slope,V=se2,random=~1|taxon,data=ma_data2)
 meta2
 
-#with modera
+#with moderators: latitude
 ma_data2$se2<-ma_data2$se^2
-meta3<-rma.mv(yi=slope,V=se2,mods=~latitude + temperature, random=~1|taxon,data=ma_data2)
+meta3<-rma.mv(yi=slope,V=se2,mods=~latitude, random=~1|taxon,data=ma_data2)
 summary(meta3)
+
+#with moderators: latitude + temperature
+meta4<-rma.mv(yi=slope,V=se2,mods=~latitude + temperature, random=~1|taxon,data=ma_data2)
+summary(meta4)
 
 #forest plot, categorising by location
 #apply colors to match the dataset
@@ -217,7 +221,7 @@ location_colors <- c("nunavut_archipelago" = "palevioletred1",
 row_colours <- location_colors[ma_data2$location]
 
 #forest plot
-par(xpd = TRUE, mar = c(5, 4, 4, 7))
+par(xpd = TRUE, mar = c(5, 4, 4, 9))
 forest(meta1, cex.lab = 0.8, cex.axis = 0.8, addfit = TRUE, shade = "zebra", slab = ma_data2$taxon, header = "Taxon", colout = row_colours, col = "palevioletred2")  
 abline(v = 0, col = "black", lwd = 1, lty = 2) 
 
@@ -226,18 +230,21 @@ custom_titles <- c("Nunavut Archipelago",
                    "Hokkaido", 
                    "Zackenberg", 
                    "Gothic")
-
 legend("right",       
        legend = custom_titles,   
        col = location_colors, 
        pch = 15,                
-       cex = 0.6, 
+       cex = 0.8, 
        bty = "n",                
-       inset = c(-0.18, 0))      
+       inset = c(-0.32, 0))      
 
-
-#latitude
+quartz()
+par(mfrow=c(1,2))
+#latitude against slope w/o temp 
 regplot(meta3,mod="latitude",bg="lightblue1",ylab="Effect size (days/°C)", xlab="Latitude")
+
+#latitude against slope w/ temp
+regplot(meta4,mod="latitude",bg="honeydew2",ylab="Effect size (days/°C)", xlab="Latitude")
 
 
 #funnel
